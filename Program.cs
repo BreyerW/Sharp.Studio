7ed7926bc518e60c307cfa13f10a290da0292ed4
damian <damian199393@o2.pl>
2021-07-11 22:39:24 +0200

    First commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Sharp.Studio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
