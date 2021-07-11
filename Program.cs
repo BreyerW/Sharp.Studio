e388588425c9fbca3a9339fbff695dd00c9965e6
damian <damian199393@o2.pl>
2021-07-11 22:34:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sharp.Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
