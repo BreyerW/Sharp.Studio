8ceaccb036e04caad91a8d9d79a8f43c9b0f2c09
dawyka <d.wyka@dasl.pl>
2023-05-20 21:41:44 +0200

    save

--- Program.cs
+++ Program.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using System.Linq;
+using System.Numerics;
+//using ImPlotNET;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+using ImGuiNET;
+using Silk.NET.Windowing.Extensions.Veldrid;
+using Sharp.Studio.View;
 
 namespace Sharp.Studio
 {
     class Program
     {
+        private static Sdl2Window _window;
+        private static GraphicsDevice _gd;
+        private static CommandList _cl;
+        private static ImGuiRenderManager _controller;
+        //private static MemoryEditor _memoryEditor;
+
+        private static Vector3 _clearColor = new Vector3(0.45f, 0.55f, 0.6f);
+        private static bool _showImGuiDemoWindow = true;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            // Create window, GraphicsDevice, and all resources necessary for the demo.
+            VeldridStartup.CreateWindowAndGraphicsDevice(
+                new WindowCreateInfo(50, 50, 1280, 720, WindowState.Hidden
+                , "ImGui.NET Sample Program"),
+                new GraphicsDeviceOptions(true, null, true, ResourceBindingModel.Improved, true, true),
+                out _window,
+                out _gd);
+
+
+            _cl = _gd.ResourceFactory.CreateCommandList();
+            _controller = new ImGuiRenderManager(_gd, _window, _gd.MainSwapchain.Framebuffer.OutputDescription, _window.Width, _window.Height);
+
+            var platform = ImGui.GetPlatformIO();
+            _window.Resized += () =>
+            {
+                _gd.MainSwapchain.Resize((uint)_window.Width, (uint)_window.Height);
+                _controller.WindowResized(_window.Width, _window.Height);
+            };
+            var view = new MainView();
+
+            // Main application loop
+            while (_window.Exists)
+            {
+                InputSnapshot snapshot = _window.PumpEvents();
+                if (!_window.Exists) { break; }
+                _controller.Update(1f / 60f, snapshot); // Feed the input events to our ImGui controller, which passes them through to ImGui.
+
+                view.OnGUI();
+                ImGui.ShowDemoWindow(ref _showImGuiDemoWindow);
+                ImGui.Begin("test");
+                
+
+                ImGui.Text("blaba");
+                ImGui.End();
+                _cl.Begin();
+                _cl.SetFramebuffer(_gd.MainSwapchain.Framebuffer);
+                _cl.ClearColorTarget(0, new RgbaFloat(_clearColor.X, _clearColor.Y, _clearColor.Z, 1f));
+                _controller.Render(_gd, _cl);
+                _cl.End();
+                _gd.SubmitCommands(_cl);
+                _gd.SwapBuffers(_gd.MainSwapchain);
+                _controller.SwapExtraWindows(_gd);
+            }
+
+            // Clean up Veldrid resources
+            _gd.WaitForIdle();
+            _controller.Dispose();
+            _cl.Dispose();
+            _gd.Dispose();
         }
     }
 }
---
+++ View/MainView.cs
@@ -0,0 +1,36 @@
+﻿using ImGuiNET;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharp.Studio.View
+{
+    public class MainView : ImGuiView
+    {
+
+        public override void OnGUI()
+        {
+            //ImGui.SetNextWindowViewport(platform.MainViewport.ID);
+            ImGui.SetNextWindowSize(new Vector2(1280, 720), ImGuiCond.FirstUseEver);
+            ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new Vector2(0));
+            ImGui.PushStyleVar(ImGuiStyleVar.WindowRounding, 0);
+            ImGui.PushStyleVar(ImGuiStyleVar.WindowBorderSize, 0);
+            ImGui.Begin("Sharp Studio", ImGuiWindowFlags.MenuBar | ImGuiWindowFlags.DockNodeHost | ImGuiWindowFlags.NoDocking);
+            ImGui.PopStyleVar(3);
+
+            var size = ImGui.GetWindowViewport().Size;
+
+            ImGui.BeginMenuBar();
+            ImGui.MenuItem("New");
+            ImGui.EndMenuBar();
+            var id = ImGui.GetID("_main_dockspace");Debug.WriteLine(ImGui.IsWindowDocked());
+            ImGui.DockSpace(id, new Vector2(size.X, size.Y - ImGui.GetFrameHeight() * 3));
+            
+            ImGui.End();
+        }
+    }
+}
